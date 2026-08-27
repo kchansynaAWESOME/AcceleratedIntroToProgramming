e55af8b67ace36146d2b1bb13c5b891bae40e113
kchansyna <kchansyna@wpi.edu>
2026-08-27 14:13:48 -0400

    added the undoDiff and applyAddDiffs functions

--- src/Labs/HW1.kt
+++ src/Labs/HW1.kt
@@ -76,27 +76,81 @@ data class Document (val author : Author,
  * | STUBS
  * ========================================== */
 
-// Consumes a piece of text and returns the text after Diff is applied
-// @param initText Initial text before Diff is applied
-// @param diff Diff object that applied to the given text
-// @return The initial text after the diff is applied
-// @throw IndexOutOfBoundsException if the Diff's position is invalid
-// @throw NoSuchElementException if the Delete Diff cannot find the the text2Delete at the given position
+/** Consumes a piece of text and returns the text after Diff is applied
+ * @param initText Initial text before Diff is applied
+ * @param diff Diff object that applied to the given text
+ * @return The initial text after the diff is applied
+ * @throw IndexOutOfBoundsException if the Diff's position is invalid
+ * @throw NoSuchElementException if the Delete Diff cannot find the the text2Delete at the given position
+ */
 //fun applyDiff(initText:String,diff:Diff) : String {
-//    return "" //STUB --TODO
+//    return "" //STUB
+//}
+
+/** undoDiff: consumes text that was affected by a Diff and "undoes" it in a sense.
+ * Should only consume the diff that was applied to the text beforehand, and should always return the unaffected text
+ * @param text the text after the diff was applied
+ * @param diff the diff object applied to the text beforehand
+ * @return the original text before the diff was applied
+ */
+//fun undoDiff(text:String,diff: Diff) : String {
+//    return "" // STUB
+//}
+
+/** applyAllDiffs: consumes raw text and a [DiffList] and applies every diff in the list to the text
+ * @param text
+ * @param diffList
+ * @return final text with all diffs applied
+ */
+//fun applyAllDiffs(text:String,diffList: DiffList) : String {
+//    return "" // STUB
 //}
 
 /* ==========================================
  * | TESTS
  * ========================================== */
 fun main() {
-    applyDiff("I LOVE THIS",Diff.Insert(1," DONT")) shouldBe "I DONT LOVE THIS"
-    shouldThrow<IndexOutOfBoundsException> {applyDiff("",Diff.Insert(1,""))}
-    shouldThrow<IndexOutOfBoundsException> {applyDiff("I LOVE THIS",Diff.Insert(-20," DONT"))}
-    applyDiff("I REALLY LOVE THIS",Diff.Delete(1," REALLY",true)) shouldBe "I LOVE THIS"
-    applyDiff("I REALLY LOVE THIS",Diff.Delete(1," REALLY",false)) shouldBe "I        LOVE THIS"
-    shouldThrow<NoSuchElementException> {applyDiff("THIS IS THE BEST",Diff.Delete(1," REALLY",true))}
-    applyDiff("hi",Diff.Delete(0,"hi",true)) shouldBe ""
+    applyDiff("I LOVE THIS",Insert(1," DONT")) shouldBe "I DONT LOVE THIS"
+    shouldThrow<IndexOutOfBoundsException> {applyDiff("",Insert(1,""))}
+    shouldThrow<IndexOutOfBoundsException> {applyDiff("I LOVE THIS",Insert(-20," DONT"))}
+    applyDiff("I REALLY LOVE THIS",Delete(1," REALLY",true)) shouldBe "I LOVE THIS"
+    applyDiff("I REALLY LOVE THIS",Delete(1," REALLY",false)) shouldBe "I        LOVE THIS"
+    shouldThrow<NoSuchElementException> {applyDiff("THIS IS THE BEST",Delete(1," REALLY",true))}
+    applyDiff("hi",Delete(0,"hi",true)) shouldBe ""
+
+    undoDiff("I DONT LOVE THIS",Insert(1," DONT")) shouldBe "I LOVE THIS"
+    undoDiff("I",Insert(0,"I")) shouldBe ""
+    undoDiff("I LOVE THIS", Delete(1," DONT",true)) shouldBe "I DONT LOVE THIS"
+    undoDiff("", Delete(0,"DONT",true)) shouldBe "DONT"
+
+    applyAllDiffs("attention all students i hate my job",
+        Dnode(
+            Insert(24," really"), Dnode(
+                Insert(31,", really"),Dempty))) shouldBe "attention all students i really, really hate my job"
+    applyAllDiffs("I LOVE THIS", Dempty) shouldBe "I LOVE THIS"
+    applyAllDiffs("I LOVE THIS", Dnode(
+        Insert(1," DONT"), Dempty)) shouldBe "I DONT LOVE THIS"
+    applyAllDiffs("I REALLY LOVE THIS", Dnode(
+        Delete(1," REALLY",true), Dempty)) shouldBe "I LOVE THIS"
+    applyAllDiffs("I REALLY LOVE THIS", Dnode(
+        Delete(1," REALLY",false), Dempty)) shouldBe "I        LOVE THIS"
+    applyAllDiffs("I LOVE THIS", Dnode(
+        Insert(1," REALLY"), Dnode(
+            Delete(2,"REALLY",true), Dnode(
+                Insert(1," DONT"), Dempty)))) shouldBe "I DONT  LOVE THIS"
+    shouldThrow<NoSuchElementException> {
+        applyAllDiffs("I LOVE THIS", Dnode(
+            Insert(1," DONT"), Dnode(
+                Delete(1," REALLY",true), Dempty)))
+    }
+    shouldThrow<IndexOutOfBoundsException> {
+        applyAllDiffs("I LOVE THIS", Dnode(
+            Insert(1," DONT"), Dnode(
+                Insert(100,"!"), Dempty)))
+    }
+    applyAllDiffs("hi", Dnode(
+        Delete(0,"hi",true), Dnode(
+            Insert(0,"bye"), Dempty))) shouldBe "bye"
 }
 
 /* ==========================================
@@ -105,12 +159,13 @@ fun main() {
 
 // Task - 1 : applyDiff
 
-// Consumes a piece of text and returns the text after Diff is applied
-// @param initText Initial text before Diff is applied
-// @param diff Diff object that applied to the given text
-// @return The initial text after the diff is applied
-// @throw IndexOutOfBoundsException if the Diff's position is invalid
-// @throw NoSuchElementException if the Delete Diff cannot find the the text2Delete at the given position
+/** Consumes a piece of text and returns the text after Diff is applied
+ * @param initText Initial text before Diff is applied
+ * @param diff Diff object that applied to the given text
+ * @return The initial text after the diff is applied
+ * @throw IndexOutOfBoundsException if the Diff's position is invalid
+ * @throw NoSuchElementException if the Delete Diff cannot find the the text2Delete at the given position
+ */
 fun applyDiff(initText:String,diff:Diff) : String {
     return when (diff) {
         is Insert -> applyInsert(initText, diff)
@@ -127,21 +182,46 @@ fun applyDelete(initText: String, diff: Delete): String {
     if (diff.position < 0 || diff.position > initText.length) throw IndexOutOfBoundsException("out of bounds")
 
     val deleteEnd = min(diff.position + diff.text2Delete.length, initText.length)
-    if (initText.substring(diff.position, deleteEnd) != diff.text2Delete) throw NoSuchElementException("no")
+    if (initText.substring(diff.position, deleteEnd) != diff.text2Delete) throw NoSuchElementException("could not find text to delete")
 
     val replacement = if (!diff.shouldCondense) " ".repeat(diff.text2Delete.length) else ""
     return initText.substring(0, diff.position) + replacement + initText.substring(deleteEnd)
 }
 
-// you dont wanna know
-fun textOccurs(mainText:String,checkText:String) : String {
-    return ""
-}
-
 
 // Task - 2 : undoDiff
 
+/** undoDiff: consumes text that was affected by a Diff and "undoes" it in a sense.
+ * Should only consume the diff that was applied to the text beforehand, and should always return the unaffected text
+ * @param text the text after the diff was applied
+ * @param diff the diff object applied to the text beforehand
+ * @return the original text before the diff was applied
+ */
+fun undoDiff(text:String,diff: Diff) : String {
+    return when (diff) {
+        is Insert -> applyDelete(text,Delete(diff.position,diff.text2Insert,true))
+        is Delete -> applyInsert(text,Insert(diff.position,diff.text2Delete))
+    }
+}
+
 // Task - 3 : applyAllDiffs
+
+/** applyAllDiffs: consumes raw text and a [DiffList] and applies every diff in the list to the text
+ * @param text
+ * @param diffList
+ * @return final text with all diffs applied
+ */
+fun applyAllDiffs(text:String,diffList: DiffList) : String {
+    return when (diffList) {
+        is Dempty -> text
+        is Dnode -> {
+            when (diffList.first) {
+                is Insert -> applyAllDiffs(applyInsert(text,diffList.first),diffList.rest)
+                is Delete -> applyAllDiffs(applyDelete(text,diffList.first),diffList.rest)
+            }
+        }
+    }
+}
 
 /* ==========================================
  * | Part - 2
